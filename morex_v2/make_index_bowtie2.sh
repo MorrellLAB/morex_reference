2fbb591c4b3cb610a3801ad087873d6699558fb5
ChaochihL <liux1299@umn.edu>
2019-06-14 17:07:41 -0500

    index ref

---
+++ morex_v2/make_index_bowtie2.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#PBS -l mem=22gb,nodes=1:ppn=4,walltime=06:00:00
+#PBS -m abe
+#PBS -M liux1299@umn.edu
+#PBS -q mesabi
+
+module load bowtie2/2.3.4.1
+
+# User provided input arguments
+REF_DIR=/panfs/roc/groups/9/morrellp/shared/References/Reference_Sequences/Barley/Morex_v2
+REF_FILENAME=Barley_Morex_V2_pseudomolecules.fasta
+
+# Go into reference dir
+cd ${REF_DIR}
+
+# Generate database prefix
+PREFIX=$(basename ${REF_FILENAME} .fasta)
+
+# Create bowtie2 index database
+bowtie2-build ${REF_DIR}/${REF_FILENAME} ${PREFIX}
+
+# Check the content of the database
+bowtie2-inspect -n ${PREFIX}
---
+++ morex_v2/make_index_bwa.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#PBS -l mem=22gb,nodes=1:ppn=4,walltime=03:00:00
+#PBS -m abe
+#PBS -M liux1299@umn.edu
+#PBS -q mesabi
+
+module load bwa/0.7.17
+
+# User provided input arguments
+REF_DIR=/panfs/roc/groups/9/morrellp/shared/References/Reference_Sequences/Barley/Morex_v2
+REF_FILENAME=Barley_Morex_V2_pseudomolecules.fasta
+
+cd ${REF_DIR}
+
+bwa index ${REF_DIR}/${REF_FILENAME}
