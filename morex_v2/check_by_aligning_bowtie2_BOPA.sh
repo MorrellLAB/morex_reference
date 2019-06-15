637f9800f489c6fd0f78e7f6378a7e1453e82818
ChaochihL <liux1299@umn.edu>
2019-06-15 10:35:51 -0500

    quick test alignment

---
+++ morex_v2/check_by_aligning_bowtie2_BOPA.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#PBS -l mem=22gb,nodes=1:ppn=6,walltime=06:00:00
+#PBS -m abe
+#PBS -M liux1299@umn.edu
+#PBS -q mesabi
+
+module load bowtie2/2.3.4.1
+
+# This script does a quick alignment as a check for the reference genome
+# Following what Li did for morex v1: https://github.com/lilei1/9k_BOPA_SNP/blob/master/script/commandlines
+
+# User provided input arguments
+FASTA_FILE=/panfs/roc/groups/9/morrellp/shared/Datasets/Genotyping/Contextual_Sequences/BOPA_SNPs/BOPA_SNPs.fasta
+DB_NAME=/panfs/roc/groups/9/morrellp/shared/References/Reference_Sequences/Barley/Morex_v2/Barley_Morex_V2_pseudomolecules
+OUT_PREFIX=BOPA_contextual_morex_v2-bowtie2
+OUT_DIR=~/scratch/barley_ref
+
+# Check if our dir exists, if not make it
+mkdir -p ${OUT_DIR}
+
+# Go into reference dir
+cd ${OUT_DIR}
+
+# Align with bowtie2
+bowtie2 -f ${FASTA_FILE} -x ${DB_NAME} -S ${OUT_PREFIX}.sam
