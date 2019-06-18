4b416abf592a2f23cc0692b2f30cff26f9b4f0c3
ChaochihL <liux1299@umn.edu>
2019-06-18 10:28:34 -0500

    working version for bowtie2 indexing

--- morex_v2/make_index_pseudo_bowtie2.sh
+++ morex_v2/make_index_pseudo_bowtie2.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#PBS -l mem=22gb,nodes=1:ppn=4,walltime=06:00:00
+#PBS -l mem=48gb,nodes=1:ppn=4,walltime=06:00:00
 #PBS -m abe
 #PBS -M liux1299@umn.edu
 #PBS -q mesabi
@@ -7,7 +7,7 @@
 set -e
 set -o pipefail
 
-module load bowtie2/2.3.4.1
+module load bowtie2/2.3.2
 
 # User provided input arguments
 REF_DIR=/panfs/roc/groups/9/morrellp/shared/References/Reference_Sequences/Barley/Morex_v2
@@ -20,7 +20,7 @@ cd ${REF_DIR}
 PREFIX=$(basename ${REF_FILENAME} .fasta)
 
 # Create bowtie2 index database
-bowtie2-build ${REF_DIR}/${REF_FILENAME} ${PREFIX}_db
+bowtie2-build ${REF_DIR}/${REF_FILENAME} ${PREFIX}
 
 # Check the content of the database
-bowtie2-inspect -n ${PREFIX}_db
+bowtie2-inspect -n ${PREFIX}
