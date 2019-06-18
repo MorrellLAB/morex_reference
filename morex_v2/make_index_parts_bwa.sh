f1f2961de7f43d716862b564933573d7b26d09d7
ChaochihL <liux1299@umn.edu>
2019-06-18 16:31:26 -0500

    index parts reference

---
+++ morex_v2/make_index_parts_bwa.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#PBS -l mem=22gb,nodes=1:ppn=4,walltime=03:00:00
+#PBS -m abe
+#PBS -M liux1299@umn.edu
+#PBS -q mesabi
+
+set -e
+set -o pipefail
+
+module load bwa/0.7.17
+
+# User provided input arguments
+REF_DIR=/panfs/roc/groups/9/morrellp/shared/References/Reference_Sequences/Barley/Morex_v2
+REF_FILENAME=Barley_Morex_V2_pseudomolecules_parts.fasta
+
+cd ${REF_DIR}
+
+bwa index ${REF_DIR}/${REF_FILENAME}
