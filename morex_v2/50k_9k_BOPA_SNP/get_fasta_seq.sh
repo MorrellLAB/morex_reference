f6fcc0410aabcf3a18cf2e2435c336edcb46d230
ChaochihL <liux1299@umn.edu>
2019-07-19 09:48:08 -0700

    script to create fasta file for failed SNPs output from SNP_Utils

---
+++ morex_v2/50k_9k_BOPA_SNP/get_fasta_seq.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+set -e
+set -o pipefail
+
+# User provided input arguments
+SNP_LIST=$1
+# File extension of SNP_LIST. Ex: .log, .txt
+SNP_LIST_EXT=$2
+CONTEXTUAL_FASTA=$3
+OUT_DIR=$4
+
+# Check if out dir exists, if not make it
+mkdir -p ${OUT_DIR}
+
+# Store SNP list in array
+SNP_ARR=($(cat ${SNP_LIST}))
+
+# Extract sequence for SNPs
+for i in ${SNP_ARR[@]}
+do
+    prefix=$(basename ${SNP_LIST} ${SNP_LIST_EXT})
+    # Identify line number of SNP in FASTA file
+    tmp_line=$(grep -n ${i} ${CONTEXTUAL_FASTA} | cut -d':' -f 1)
+    tmp_seq_line=$((${tmp_line}+1))
+    # Extract sequence associated with SNP
+    sed -n ${tmp_line},${tmp_seq_line}p ${CONTEXTUAL_FASTA} >> ${OUT_DIR}/${prefix}.fasta
+done
