cc23afd24eb627ae68833496fcbc4975d42a4e97
ChaochihL <liux1299@umn.edu>
2021-09-20 16:48:31 -0500

    script used to prepare files

---
+++ morex_v3/50k_9k_BOPA_SNP/remove_duplicates_from_vcf.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+set -e
+set -o pipefail
+
+# Dependencies
+module load python3/3.8.3_anaconda2020.07_mamba
+
+# User provided input arguments
+VCF="$1"
+PREFIX="$2"
+OUT_DIR="$3"
+
+# These are set for Morex v3
+PSEUDO_TO_PARTS_SCRIPT="/panfs/roc/groups/9/morrellp/liux1299/GitHub/File_Conversions/Barley_Pseudomolecules_to_Parts.py"
+# See script ~/GitHub/File_Conversions/Barley_Pseudomolecules_to_Parts.py for a list of valid options
+REF_VERSION="morex_v3"
+
+#------------------------
+# Check if out dir and out subdir exist, if not make them
+mkdir -p ${OUT_DIR} ${OUT_DIR}/duplicate_snps
+
+# Make a list of duplicate SNP names
+grep -v "#" ${VCF} | cut -f 3 | sort | uniq -c | sort -n -r | grep -vw "1" | sed -e 's,      ,,' | cut -d' ' -f 2 | sort -V > ${OUT_DIR}/duplicate_snps/${PREFIX}_duplicate_snp_names.txt
+# Save duplicates to vcf
+grep -f ${OUT_DIR}/duplicate_snps/${PREFIX}_duplicate_snp_names.txt ${VCF} | sort -V -k3,3 > ${OUT_DIR}/duplicate_snps/${PREFIX}_duplicates.vcf
+# Make a copy for resolving duplicates
+cp ${OUT_DIR}/duplicate_snps/${PREFIX}_duplicates.vcf ${OUT_DIR}/duplicate_snps/${PREFIX}_duplicates_resolved.vcf
+
+# Remove duplicates from main VCF
+grep -vf ${OUT_DIR}/duplicate_snps/${PREFIX}_duplicate_snp_names.txt ${VCF} > ${OUT_DIR}/${PREFIX}_noRescuedSNPs.vcf
+
+# Convert pseudomolecular to parts positions
+${PSEUDO_TO_PARTS_SCRIPT} --vcf ${OUT_DIR}/${PREFIX}_noRescuedSNPs.vcf ${REF_VERSION} > ${OUT_DIR}/${PREFIX}_noRescuedSNPs_partsRef.vcf
